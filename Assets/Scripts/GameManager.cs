1e43239051c63f7f46cd6e8a164e023cba3160a1
Mikayil Quenum <mquenum@users.noreply.github.com>
2023-01-06 16:29:39 +0100

    Way too convoluted way of changing object to appear, but still, it is working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private bool _cube;
+    [SerializeField] private bool _sphere;
+    [SerializeField] private bool _capsule;
+    [SerializeField] private bool _cylinder;
+    [SerializeField] private bool _plane;
+
     void Update()
     {
         CubeOnClick();
@@ -23,17 +32,102 @@ public class GameManager : MonoBehaviour
             // create a variable where we transform screen coordinates in space coordinates (relative to game view)
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-            // cube creation
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // obj creation
+            // TODO: LE CODE LE PLUS DEGUEULASSE DU MONDE, A REVOIR... MAIS COMMENT ?
+            GameObject obj;
+            if (_cube)
+            {
+                _prefab = null;
+                _sphere = false;
+                _capsule = false;
+                _cylinder = false;
+                _plane = false;
 
-            // change the cube scale
-            cube.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+                obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-            // position the cube
-            cube.transform.position = new Vector3(0, worldPosition.y, worldPosition.z);
+                SetupObj(obj, worldPosition);
+            }
+            else if (_sphere)
+            {
+                _prefab = null;
+                _cube = false;
+                _capsule = false;
+                _cylinder = false;
+                _plane = false;
 
-            // set a color to the cube
-            cube.GetComponent<Renderer>().material.color = Color.red;
+                obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+                SetupObj(obj, worldPosition);
+            }
+            else if (_capsule)
+            {
+                _prefab = null;
+                _cube = false;
+                _sphere = false;
+                _cylinder = false;
+                _plane = false;
+
+                obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+
+                SetupObj(obj, worldPosition);
+            }
+            else if (_cylinder)
+            {
+                _prefab = null;
+                _cube = false;
+                _sphere = false;
+                _capsule = false;
+                _plane = false;
+
+                obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+                SetupObj(obj, worldPosition);
+            }
+            else if (_plane)
+            {
+                _prefab = null;
+                _cube = false;
+                _sphere = false;
+                _capsule = false;
+                _cylinder = false;
+
+                obj = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+                SetupObj(obj, worldPosition);
+            }
+            else if (_prefab)
+            {
+                _cube = false;
+                _sphere = false;
+                _capsule = false;
+                _cylinder = false;
+                _plane = false;
+
+                obj = _prefab.gameObject;
+
+                SetupObj(obj, worldPosition);
+            }
+        }
+    }
+
+    private void SetupObj(GameObject obj, Vector3 worldPosition)
+    {
+        // change the obj scale
+        obj.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+
+        // position the obj
+        obj.transform.position = new Vector3(0, worldPosition.y, worldPosition.z);
+
+        if (_prefab == null)
+        {
+            // set a color to the obj
+            obj.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (_prefab)
+        {
+            GameObject instPrefab = GameObject.Instantiate(_prefab, new Vector3(0, worldPosition.y, worldPosition.z), Quaternion.identity);
+            instPrefab.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
 }
